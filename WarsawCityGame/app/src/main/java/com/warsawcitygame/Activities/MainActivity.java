58e0bc07704e9d297cc760afe16c753f27360266
Tygrysku <duszakp@student.mini.pw.edu.pl>
2016-04-12 17:24:36 +0200

    Some achievements fragment (looks kinda meh but tried to to sth more than listview. any idea what to do here?

--- WarsawCityGame/app/src/main/java/com/warsawcitygame/Activities/MainActivity.java
+++ WarsawCityGame/app/src/main/java/com/warsawcitygame/Activities/MainActivity.java
@@ -71,7 +71,7 @@ public class MainActivity extends Activity {
                 // Photos
                 navDrawerItems.add(new NavDrawerItem(navMenuTitles[2], navMenuIcons.getResourceId(2, -1)));
                 // Communities, Will add a counter here
-                navDrawerItems.add(new NavDrawerItem(navMenuTitles[3], navMenuIcons.getResourceId(3, -1), true, "22"));
+                navDrawerItems.add(new NavDrawerItem(navMenuTitles[3], navMenuIcons.getResourceId(3, -1), true, "10"));
                 // Pages
                 navDrawerItems.add(new NavDrawerItem(navMenuTitles[4], navMenuIcons.getResourceId(4, -1)));
                 // What's hot, We  will add a counter here
--- WarsawCityGame/app/src/main/java/com/warsawcitygame/Fragments/AchievementsFragment.java
+++ WarsawCityGame/app/src/main/java/com/warsawcitygame/Fragments/AchievementsFragment.java
@@ -5,11 +5,20 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.GridView;
 
+import com.warsawcitygame.Adapters.GridViewAdapter;
 import com.warsawcitygame.R;
 
+import java.util.ArrayList;
+
 public class AchievementsFragment extends Fragment {
-	
+
+    private GridViewAdapter mAdapter;
+    private ArrayList<String> listCountry;
+    private ArrayList<Integer> listFlag;
+
+    private GridView gridView;
 	public AchievementsFragment(){}
 	
 	@Override
@@ -17,7 +26,47 @@ public class AchievementsFragment extends Fragment {
             Bundle savedInstanceState) {
  
         View rootView = inflater.inflate(R.layout.fragment_achievements, container, false);
-         
+        prepareList();
+
+        // prepared arraylist and passed it to the Adapter class
+        mAdapter = new GridViewAdapter(getActivity(),listCountry, listFlag);
+
+        // Set custom adapter to gridview
+        gridView = (GridView)rootView.findViewById(R.id.gridView1);
+
+        gridView.setAdapter(mAdapter);
         return rootView;
+    }
+
+    public void prepareList()
+    {
+        listCountry = new ArrayList<>();
+
+        listCountry.add("Account created");
+        listCountry.add("Logged in for a very first time");
+        listCountry.add("Jak Wenus i Mars jesteśmy z innych gwiazd");
+        listCountry.add("Estoy cantando pero mi nombre es Pablito");
+        listCountry.add("Traveled over 10 kilometers");
+        listCountry.add("Fala akustyczna przemieszcza się w powietrzu w postaci lokalnych zagęszczeń i rozrzedzeń ośrodka");
+        listCountry.add("Sunday morning rain is fallin, steal some covers share some skin");
+        listCountry.add("I've got the move like Jagger, I've got the mooooove like Jagger");
+        listCountry.add("Can you feel the love tonight");
+        listCountry.add("Jak Wenus i Mars jesteśmy z innych gwiazd");
+        listCountry.add("Jak Wenus i Mars jesteśmy z innych gwiazd");
+
+
+        listFlag = new ArrayList<>();
+        listFlag.add(R.drawable.acccreated);
+        listFlag.add(R.drawable.loggedin);
+        listFlag.add(R.drawable.dontknow);
+        listFlag.add(R.drawable.dontknow);
+        listFlag.add(R.drawable.dontknow);
+        listFlag.add(R.drawable.dontknow);
+        listFlag.add(R.drawable.dontknow);
+        listFlag.add(R.drawable.dontknow);
+        listFlag.add(R.drawable.dontknow);
+        listFlag.add(R.drawable.dontknow);
+        listFlag.add(R.drawable.dontknow);
+
     }
 }
