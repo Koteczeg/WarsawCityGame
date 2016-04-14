2705bd8550a4ce53d2796d5995e9cac0c05ab286
Mateusz Jabłoński <matjablonski94@gmail.com>
2016-04-14 15:39:54 +0200

    aborting current mission

--- WarsawCityGame/app/src/main/java/com/warsawcitygame/Fragments/CurrentMissionFragment.java
+++ WarsawCityGame/app/src/main/java/com/warsawcitygame/Fragments/CurrentMissionFragment.java
@@ -1,6 +1,7 @@
 package com.warsawcitygame.Fragments;
 
 import android.app.Fragment;
+import android.app.FragmentManager;
 import android.content.DialogInterface;
 import android.os.Bundle;
 import android.view.LayoutInflater;
@@ -18,11 +19,23 @@ public class CurrentMissionFragment extends Fragment {
 	public CurrentMissionFragment(){}
 	
 	@Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+    public View onCreateView( final LayoutInflater inflater,final ViewGroup container,
             Bundle savedInstanceState) {
  
         View rootView = inflater.inflate(R.layout.fragment_current_mission, container, false);
 
+        Button button = (Button) rootView.findViewById(R.id.abortButton);
+        button.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View v)
+            {
+                Fragment fragment = new BlankCurrentMissionFragment();
+                FragmentManager fragmentManager = getFragmentManager();
+                fragmentManager.beginTransaction()
+                        .replace(R.id.frame_container, fragment).commit();
+            }
+        });
         return rootView;
     }
 
