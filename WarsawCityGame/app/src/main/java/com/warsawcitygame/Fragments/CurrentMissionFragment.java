7711e08474d725abbbcb30e450f910ebddb6148a
Mateusz Jabłoński <matjablonski94@gmail.com>
2016-04-14 15:11:50 +0200

    adding basic view for current mission

--- WarsawCityGame/app/src/main/java/com/warsawcitygame/Fragments/CurrentMissionFragment.java
+++ WarsawCityGame/app/src/main/java/com/warsawcitygame/Fragments/CurrentMissionFragment.java
@@ -1,12 +1,17 @@
 package com.warsawcitygame.Fragments;
 
 import android.app.Fragment;
+import android.content.DialogInterface;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
 
 import com.warsawcitygame.R;
+
+import static android.content.DialogInterface.*;
 
 public class CurrentMissionFragment extends Fragment {
 	
@@ -17,7 +22,14 @@ public class CurrentMissionFragment extends Fragment {
             Bundle savedInstanceState) {
  
         View rootView = inflater.inflate(R.layout.fragment_current_mission, container, false);
-         
+
         return rootView;
     }
+
+
+
+
+
+
+
 }
