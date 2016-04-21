e0315bce9a1f0da787ecd27ebf8569b38b9bc936
Tygrysku <duszakp@student.mini.pw.edu.pl>
2016-04-21 23:48:14 +0200

    deploying API server. some testing via controller

---
+++ WarsawCityGameServer/WarsawCityGamesServer.Services/Controllers/TestController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using WarsawCityGamesServer.Entities.Context;
+
+namespace WarsawCityGamesServer.Services.Controllers
+{
+    [RoutePrefix("Test")]
+    public class TestController : ApiController
+    {
+        [Route("Sample"), HttpGet]
+        public IHttpActionResult GetAddress()
+        {
+            return Ok("ikarłapałraki");
+        }
+    }
+}
