a22937ed3373ebc44227343bcd85f6ae6aeb950f
Mateusz Bąkała <bakala12@o2.pl>
2016-04-21 14:51:40 +0200

    Adding EntityFramework migrations

---
+++ WarsawCityGameServer/WarsawCityGamesServer.Entities/Context/CityGamesContext.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WarsawCityGamesServer.Entities.Entities;
+
+namespace WarsawCityGamesServer.Entities.Context
+{
+    public class CityGamesContext : DbContext
+    {
+        public IDbSet<Level> Levels { get; set; } 
+    }
+}
---
+++ WarsawCityGameServer/WarsawCityGamesServer.Entities/Migrations/Configuration.cs
@@ -0,0 +1,31 @@
+namespace WarsawCityGamesServer.Entities.Migrations
+{
+    using System;
+    using System.Data.Entity;
+    using System.Data.Entity.Migrations;
+    using System.Linq;
+
+    internal sealed class Configuration : DbMigrationsConfiguration<WarsawCityGamesServer.Entities.Context.CityGamesContext>
+    {
+        public Configuration()
+        {
+            AutomaticMigrationsEnabled = false;
+        }
+
+        protected override void Seed(WarsawCityGamesServer.Entities.Context.CityGamesContext context)
+        {
+            //  This method will be called after migrating to the latest version.
+
+            //  You can use the DbSet<T>.AddOrUpdate() helper extension method 
+            //  to avoid creating duplicate seed data. E.g.
+            //
+            //    context.People.AddOrUpdate(
+            //      p => p.FullName,
+            //      new Person { FullName = "Andrew Peters" },
+            //      new Person { FullName = "Brice Lambson" },
+            //      new Person { FullName = "Rowan Miller" }
+            //    );
+            //
+        }
+    }
+}
