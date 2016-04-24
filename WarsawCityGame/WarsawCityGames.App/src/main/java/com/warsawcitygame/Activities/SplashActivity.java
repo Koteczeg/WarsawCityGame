c8c0dd75c5f1cc4bd0ce7d32256578caf6ea88d9
Tygrysku <duszakp@student.mini.pw.edu.pl>
2016-04-24 16:15:54 +0200

    splash activity refactoring

--- WarsawCityGame/WarsawCityGames.App/src/main/java/com/warsawcitygame/Activities/SplashActivity.java
+++ WarsawCityGame/WarsawCityGames.App/src/main/java/com/warsawcitygame/Activities/SplashActivity.java
@@ -7,29 +7,55 @@ import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
+import android.support.annotation.NonNull;
 import android.view.View;
 import android.view.animation.AccelerateDecelerateInterpolator;
 import android.view.animation.AlphaAnimation;
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
 
-import com.warsawcitygame.CustomControls.KenBurnsView;
 import com.warsawcitygame.R;
 
-public class SplashActivity extends Activity {
+import butterknife.Bind;
+import butterknife.ButterKnife;
 
-    private KenBurnsView mKenBurns;
-
+public class SplashActivity extends Activity
+{
+    @Bind(R.id.splashElementsLayout) View splashElementsLayout;
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
+        ButterKnife.bind(this);
         setAnimation();
         getWindow().getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_HIDE_NAVIGATION);
+        Animation fadeOut = setElementsLayoutAnimation();
+        setPostDelayLogoAnimation(fadeOut);
 
-        mKenBurns = (KenBurnsView) findViewById(com.warsawcitygame.R.id.splash_background);
-        mKenBurns.setImageResource(com.warsawcitygame.R.drawable.splash_background);
-        final View splashElementsLayout = findViewById(R.id.splashElementsLayout);
+    }
+
+    private void setPostDelayLogoAnimation(Animation fadeOut)
+    {
+        splashElementsLayout.startAnimation(fadeOut);
+        int SPLASH_TIME_OUT = 5000;
+        new Handler().postDelayed(new Runnable()
+        {
+
+            @Override
+            public void run()
+            {
+                Intent i = new Intent(getApplicationContext(), LoginActivity.class);
+                startActivity(i);
+
+                finish();
+            }
+        }, SPLASH_TIME_OUT);
+    }
+
+    @NonNull
+    private Animation setElementsLayoutAnimation()
+    {
         Animation fadeOut = new AlphaAnimation(0f, 1f);
         fadeOut.setDuration(1000);
         fadeOut.setAnimationListener(new Animation.AnimationListener() {
@@ -48,23 +74,11 @@ public class SplashActivity extends Activity {
 
             }
         });
-
-        splashElementsLayout.startAnimation(fadeOut);
-        int SPLASH_TIME_OUT = 5000;
-        new Handler().postDelayed(new Runnable() {
-
-            @Override
-            public void run() {
-                Intent i = new Intent(getApplicationContext(), LoginActivity.class);
-                startActivity(i);
-
-                finish();
-            }
-        }, SPLASH_TIME_OUT);
-
+        return fadeOut;
     }
 
-    private void setAnimation() {
+    private void setAnimation()
+    {
         ObjectAnimator scaleXAnimation = ObjectAnimator.ofFloat(findViewById(R.id.welcome_text), "scaleX", 5.0F, 1.0F);
         scaleXAnimation.setInterpolator(new AccelerateDecelerateInterpolator());
         scaleXAnimation.setDuration(1200);
@@ -78,7 +92,6 @@ public class SplashActivity extends Activity {
         animatorSet.play(scaleXAnimation).with(scaleYAnimation).with(alphaAnimation);
         animatorSet.setStartDelay(500);
         animatorSet.start();
-
         findViewById(R.id.imagelogo).setAlpha(1.0F);
         Animation anim = AnimationUtils.loadAnimation(getApplicationContext(), R.anim.translate_top_to_center);
         findViewById(R.id.imagelogo).startAnimation(anim);
