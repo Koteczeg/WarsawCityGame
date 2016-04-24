185bf0659fcd2b6fb0454aa1f72e81b3533221ea
Tygrysku <duszakp@student.mini.pw.edu.pl>
2016-04-24 15:19:26 +0200

    Main activity refactoring

--- WarsawCityGame/WarsawCityGames.App/src/main/java/com/warsawcitygame/Activities/MainActivity.java
+++ WarsawCityGame/WarsawCityGames.App/src/main/java/com/warsawcitygame/Activities/MainActivity.java
@@ -10,17 +10,14 @@ import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v4.app.ActionBarDrawerToggle;
 import android.support.v4.widget.DrawerLayout;
-import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 
-import android.widget.AdapterView;
 import android.widget.FrameLayout;
 import android.widget.ListView;
 
-import com.warsawcitygame.Context.MissionContext;
 import com.warsawcitygame.Fragments.DonateFragment;
 import com.warsawcitygame.Fragments.GetMissionFragment;
 import com.warsawcitygame.Fragments.CurrentMissionFragment;
@@ -36,121 +33,88 @@ import com.warsawcitygame.CustomControls.NavDrawerItem;
 import java.util.ArrayList;
 import java.util.List;
 
+import butterknife.Bind;
+import butterknife.ButterKnife;
+import butterknife.OnItemClick;
+
 import static com.warsawcitygame.Transitions.Animations.fadeOutAnimationNewFragment;
 
 
-public class MainActivity extends Activity {
-    private DrawerLayout mDrawerLayout;
-    private ActionBarDrawerToggle mDrawerToggle;
+public class MainActivity extends Activity
+{
+    @Bind(R.id.menu_layout) DrawerLayout menuLayout;
+    @Bind(R.id.menu_list) ListView menuList;
+    @Bind(R.id.frame_container) FrameLayout frameLayout;
 
-    private ListView mDrawerList;
-
-    // slide menu items
-    private String[] navMenuTitles;
-    private TypedArray navMenuIcons;
-
-    private ArrayList<NavDrawerItem> navDrawerItems;
-    private NavDrawerListAdapter adapter;
+    private ActionBarDrawerToggle menuToggle;
+    private Fragment activeFragment = null;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
 
-        // load slide menu items
-        navMenuTitles = getResources().getStringArray(R.array.nav_drawer_items);
-
-        // nav drawer icons from resources
-        navMenuIcons = getResources()
-                .obtainTypedArray(R.array.nav_drawer_icons);
-
-        mDrawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
-        mDrawerList = (ListView) findViewById(R.id.list_slidermenu);
-
-        navDrawerItems = new ArrayList<>();
-
-        // adding nav drawer items to array
-        // Home
-        navDrawerItems.add(new NavDrawerItem(navMenuTitles[0], navMenuIcons.getResourceId(0, -1)));
-        // Find People
-        navDrawerItems.add(new NavDrawerItem(navMenuTitles[1], navMenuIcons.getResourceId(1, -1)));
-        // Photos
-        navDrawerItems.add(new NavDrawerItem(navMenuTitles[2], navMenuIcons.getResourceId(2, -1)));
-        // Communities, Will add a counter here
-        navDrawerItems.add(new NavDrawerItem(navMenuTitles[3], navMenuIcons.getResourceId(3, -1), true, "3"));
-        // Pages
-        navDrawerItems.add(new NavDrawerItem(navMenuTitles[4], navMenuIcons.getResourceId(4, -1)));
-        // What's hot, We  will add a counter here
-        navDrawerItems.add(new NavDrawerItem(navMenuTitles[5], navMenuIcons.getResourceId(5, -1)));
-
-        navDrawerItems.add(new NavDrawerItem(navMenuTitles[6], navMenuIcons.getResourceId(6, -1)));
-        navDrawerItems.add(new NavDrawerItem(navMenuTitles[7], navMenuIcons.getResourceId(7, -1)));
-
-
-        // Recycle the typed array
-        navMenuIcons.recycle();
-
-        mDrawerList.setOnItemClickListener(new SlideMenuClickListener());
-
-        // setting the nav drawer list adapter
-        adapter = new NavDrawerListAdapter(getApplicationContext(),
-                navDrawerItems);
-        mDrawerList.setAdapter(adapter);
-
-        // enabling action bar app icon and behaving it as toggle button
-        //getSupportActionBar().setDisplayHomeAsUpEnabled(true);
-        //getActionBar().setHomeButtonEnabled(true);
-        mDrawerToggle = new ActionBarDrawerToggle(this, mDrawerLayout,
-                R.drawable.logo, //nav menu toggle icon
-                R.string.app_name, // nav drawer open - description for accessibility
-                R.string.app_name // nav drawer close - description for accessibility
-        ) {
-            public void onDrawerClosed(View view) {
-                //getActionBar().setTitle(mTitle);
-                // calling onPrepareOptionsMenu() to show action bar icons
-                invalidateOptionsMenu();
-            }
-
-            public void onDrawerOpened(View drawerView) {
-                //getActionBar().setTitle(mDrawerTitle);
-                // calling onPrepareOptionsMenu() to hide action bar icons
-                invalidateOptionsMenu();
-            }
-        };
-        mDrawerLayout.setDrawerListener(mDrawerToggle);
-
-        if (savedInstanceState == null) {
-            // on first time display view for first nav item
+        initializeMenu();
+        if (savedInstanceState == null)
+        {
             displayView(0);
         }
     }
 
-    /**
-     * Slide menu item click listener
-     */
-    private class SlideMenuClickListener implements
-            ListView.OnItemClickListener {
-        @Override
-        public void onItemClick(AdapterView<?> parent, View view, int position,
-                                long id) {
-            // display view for selected nav drawer item
-            displayView(position);
+    private void initializeMenu()
+    {
+        NavDrawerListAdapter menuAdapter = new NavDrawerListAdapter(this, getMenuElements());
+        menuList.setAdapter(menuAdapter);
+        menuToggle = new ActionBarDrawerToggle(this, menuLayout, R.drawable.logo, R.string.app_name, R.string.app_name)
+        {
+            public void onDrawerClosed(View view)
+            {
+                invalidateOptionsMenu();
+            }
+
+            public void onDrawerOpened(View drawerView)
+            {
+                invalidateOptionsMenu();
+            }
+        };
+        menuLayout.setDrawerListener(menuToggle);
+    }
+
+    private ArrayList<NavDrawerItem> getMenuElements()
+    {
+        ArrayList<NavDrawerItem> menuElements = new ArrayList<>();
+        String[] menuTitles = getResources().getStringArray(R.array.menu_titles);
+        TypedArray menuIcons = getResources().obtainTypedArray(R.array.menu_icons);
+        for(int i=0; i<8; i++)
+        {
+            menuElements.add(new NavDrawerItem(menuTitles[i], menuIcons.getResourceId(i, -1)));
         }
+        menuIcons.recycle();
+        return menuElements;
+    }
+
+    @OnItemClick(R.id.menu_list)
+    public void onMenuOptionClick(int position)
+    {
+        displayView(position);
     }
 
     @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
+    public boolean onCreateOptionsMenu(Menu menu)
+    {
         getMenuInflater().inflate(R.menu.main, menu);
         return true;
     }
 
     @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // toggle nav drawer on selecting action bar app icon/title
-        if (mDrawerToggle.onOptionsItemSelected(item)) {
+    public boolean onOptionsItemSelected(MenuItem item)
+    {
+        if (menuToggle.onOptionsItemSelected(item))
+        {
             return true;
         }
-        // Handle action bar actions click
         switch (item.getItemId()) {
             case R.id.action_settings:
                 return true;
@@ -159,152 +123,103 @@ public class MainActivity extends Activity {
         }
     }
 
-    /* *
-     * Called when invalidateOptionsMenu() is triggered
-     */
     @Override
-    public boolean onPrepareOptionsMenu(Menu menu) {
-        // if nav drawer is opened, hide the action items
-        boolean drawerOpen = mDrawerLayout.isDrawerOpen(mDrawerList);
+    public boolean onPrepareOptionsMenu(Menu menu)
+    {
+        boolean drawerOpen = menuLayout.isDrawerOpen(menuList);
         menu.findItem(R.id.action_settings).setVisible(!drawerOpen);
         return super.onPrepareOptionsMenu(menu);
     }
 
-    Fragment fragment = null;
-
-    /**
-     * Diplaying fragment view for selected nav drawer list item
-     */
-    private void displayView(int position) {
-        // update the main content by replacing fragments
-
-        final FrameLayout fl = (FrameLayout) findViewById(R.id.frame_container);
-        fl.setVisibility(View.INVISIBLE);
-        switch (position) {
+    private void displayView(int position)
+    {
+        frameLayout.setVisibility(View.INVISIBLE);
+        switch (position)
+        {
             case 0:
-                fragment = new CurrentMissionFragment();
-                fadeOutAnimationNewFragment(fl);
-
+                activeFragment = new CurrentMissionFragment();
                 break;
-            case 1: {
-                fragment = new ProfileFragment();
-                fadeOutAnimationNewFragment(fl);
+            case 1:
+                activeFragment = new ProfileFragment();
                 break;
-            }
             case 2:
-                fragment = new FriendsFragment();
-                fadeOutAnimationNewFragment(fl);
-
+                activeFragment = new FriendsFragment();
                 break;
             case 3:
-                fragment = new GetMissionFragment();
-                fadeOutAnimationNewFragment(fl);
-
+                activeFragment = new GetMissionFragment();
                 break;
             case 4:
-
-                fragment = new LoadingFragment();
-                fadeOutAnimationNewFragment(fl);
-                DownloadAchievements da = new DownloadAchievements();
-                da.execute();
-                // update selected item and title, then close the drawer
-                //mDrawerList.setItemChecked(position, true);
-                //mDrawerList.setSelection(position);
-                //setTitle(navMenuTitles[position]);
-
-//                            mDrawerLayout.closeDrawer(mDrawerList);
-//                            new Thread(new Runnable() {
-//                                    @Override
-//                                    public void run() {
-//                                            try {
-//                                                Thread.sleep(5000);
-//                                            } catch (InterruptedException e) {
-//                                                    e.printStackTrace();
-//                                            }
-//                                            fragment = new AchievementsFragment();
-//
-//                                            fadeOutAnimationNewFragment(fl);
-//                                            if (fragment != null) {
-//                                                    FragmentManager fragmentManager = getFragmentManager();
-//                                                    fragmentManager.beginTransaction()
-//                                                            .replace(R.id.frame_container, fragment).commit();
-//
-//                                                    // update selected item and title, then close the drawer
-//                                                    //mDrawerList.setItemChecked(position, true);
-//                                                    //mDrawerList.setSelection(position);
-//                                                    //setTitle(navMenuTitles[position]);
-//                                                    mDrawerLayout.closeDrawer(mDrawerList);
-//                                            } else {
-//                                                    // error in creating fragment
-//                                                    Log.e("MainActivity", "Error in creating fragment");
-//                                            }
-//                                    }
-//                            }).start();
-
-                //FragmentManager fragmentManager = getFragmentManager();
-                //fragmentManager.beginTransaction()
-                //        .replace(R.id.frame_container, fragment).commit();
-
+                activeFragment = new LoadingFragment();
+                getDataForAchievementsFragment();
                 break;
             case 5:
-                fragment = new HallOfFameFragment();
-                fadeOutAnimationNewFragment(fl);
-
+                activeFragment = new HallOfFameFragment();
                 break;
             case 6:
-                // logging off logic async somehow
-                Intent i = new Intent(getApplicationContext(), LoginActivity.class);
-                startActivity(i);
-                finish();
+                logout();
                 break;
             case 7:
-                fragment = new DonateFragment();
-                fadeOutAnimationNewFragment(fl);
-                    break;
-            default:
+                activeFragment = new DonateFragment();
                 break;
         }
+        replaceFragment(position);
+        fadeOutAnimationNewFragment(frameLayout);
+    }
 
-        if (fragment != null) {
+    private void getDataForAchievementsFragment()
+    {
+        DownloadAchievements da = new DownloadAchievements();
+        da.execute();
+    }
+
+    private void replaceFragment(int position)
+    {
+        if (activeFragment != null)
+        {
             FragmentManager fragmentManager = getFragmentManager();
-            fragmentManager.beginTransaction()
-                    .replace(R.id.frame_container, fragment).commit();
-
-            // update selected item and title, then close the drawer
-            mDrawerList.setItemChecked(position, true);
-            mDrawerList.setSelection(position);
-            setTitle(navMenuTitles[position]);
-            mDrawerLayout.closeDrawer(mDrawerList);
-        } else {
-            // error in creating fragment
-            Log.e("MainActivity", "Error in creating fragment");
+            fragmentManager.beginTransaction().replace(R.id.frame_container, activeFragment).commit();
+            menuList.setItemChecked(position, true);
+            menuList.setSelection(position);
+            menuLayout.closeDrawer(menuList);
+        }
+        else
+        {
+            Log.e("MainActivity", "Error in creating activeFragment");
         }
     }
 
-
-    /**
-     * When using the ActionBarDrawerToggle, you must call it during
-     * onPostCreate() and onConfigurationChanged()...
-     */
+    private void logout()
+    {
+        //TODO async logout
+        Intent i = new Intent(this, LoginActivity.class);
+        startActivity(i);
+        finish();
+    }
 
     @Override
-    protected void onPostCreate(Bundle savedInstanceState) {
+    protected void onPostCreate(Bundle savedInstanceState)
+    {
         super.onPostCreate(savedInstanceState);
-        // Sync the toggle state after onRestoreInstanceState has occurred.
-        mDrawerToggle.syncState();
+        menuToggle.syncState();
     }
 
     @Override
-    public void onConfigurationChanged(Configuration newConfig) {
+    public void onConfigurationChanged(Configuration newConfig)
+    {
         super.onConfigurationChanged(newConfig);
-        // Pass any configuration change to the drawer toggls
-        mDrawerToggle.onConfigurationChanged(newConfig);
+        menuToggle.onConfigurationChanged(newConfig);
     }
 
-    class DownloadAchievements extends AsyncTask<String, Void, List<String>> {
 
+
+    //TODO communication architecture
+    //TODO layer must be able to aggregate menuLayout and activeFragment (to invoke post executive)
+    //TODO or we can find another way to replace fragments?
+    class DownloadAchievements extends AsyncTask<String, Void, List<String>>
+    {
         @Override
-        protected List<String> doInBackground(String... params) {
+        protected List<String> doInBackground(String... params)
+        {
             try {
                 Thread.sleep(2000);
             } catch (InterruptedException e) {
@@ -314,25 +229,17 @@ public class MainActivity extends Activity {
         }
 
         @Override
-        protected void onPostExecute(List<String> result) {
-            fragment = new AchievementsFragment();
-            final FrameLayout fl = (FrameLayout) findViewById(R.id.frame_container);
-            fl.setVisibility(View.INVISIBLE);
-            fadeOutAnimationNewFragment(fl);
-
-            if (fragment != null) {
+        protected void onPostExecute(List<String> result)
+        {
+            activeFragment = new AchievementsFragment();
+            frameLayout.setVisibility(View.INVISIBLE);
+            fadeOutAnimationNewFragment(frameLayout);
+            if (activeFragment != null) {
                 FragmentManager fragmentManager = getFragmentManager();
-                fragmentManager.beginTransaction()
-                        .replace(R.id.frame_container, fragment).commit();
-
-                // update selected item and title, then close the drawer
-                //mDrawerList.setItemChecked(position, true);
-                //mDrawerList.setSelection(position);
-                //setTitle(navMenuTitles[position]);
-                mDrawerLayout.closeDrawer(mDrawerList);
+                fragmentManager.beginTransaction().replace(R.id.frame_container, activeFragment).commit();
+                menuLayout.closeDrawer(menuList);
             } else {
-                // error in creating fragment
-                Log.e("MainActivity", "Error in creating fragment");
+                Log.e("MainActivity", "Error in creating activeFragment");
             }
         }
     }
