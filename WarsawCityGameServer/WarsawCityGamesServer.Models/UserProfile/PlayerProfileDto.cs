bd365d39e3489ad870ae313fedbffb1d71bc2a58
Mateusz Bąkała <bakala12@o2.pl>
2016-05-04 00:22:51 +0200

    Adding controller and dto

---
+++ WarsawCityGameServer/WarsawCityGamesServer.Models/UserProfile/PlayerProfileDto.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WarsawCityGamesServer.Entities.Entities;
+
+namespace WarsawCityGamesServer.Models.UserProfile
+{
+    public class PlayerProfileDto
+    {
+        public string Email { get; set; }
+        public string Name { get; set; }
+        public int Exp { get; set; }
+        public Level Level { get; set; }
+        public string Description { get; set; }
+        public string Username { get; set; }
+        public byte[] UserImage { get; set; }
+        public string Password { get; set; }
+    }
+}
---
+++ WarsawCityGameServer/WarsawCityGamesServer.Services/Controllers/UserProfileController.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using AutoMapper;
+using Microsoft.AspNet.Identity;
+using WarsawCityGamesServer.Entities.Context;
+using WarsawCityGamesServer.Entities.Entities;
+using WarsawCityGamesServer.Models.UserProfile;
+
+namespace WarsawCityGamesServer.Services.Controllers
+{
+    [Authorize]
+    [RoutePrefix("UserProfile")]
+    public class UserProfileController : ApiController
+    {
+        private readonly CityGamesContext _context;
+        private readonly UserManager<User, string> _userManager;
+        private readonly IMapper _mapper;
+
+        public UserProfileController(CityGamesContext context, UserManager<User, string> userManager, IMapper mapper)
+        {
+            _context = context;
+            _userManager = userManager;
+            _mapper = mapper;
+        }
+
+        [Authorize]
+        [Route("GetProfileData")]
+        [HttpGet]
+        public IHttpActionResult GetProfileData()
+        {
+            PlayerProfileDto dto = new PlayerProfileDto();
+            string username = User.Identity.Name;
+            Player player = _context.Players.FirstOrDefault(x => x.User.UserName == username);
+            if (player == null)
+                return BadRequest();
+            
+            return Ok(dto);
+        }
+    }
+}
