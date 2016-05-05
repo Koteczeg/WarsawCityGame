e5e72325c21297294b765c04e3009fc6e26e2809
Mateusz Jabłoński <matjablonski94@gmail.com>
2016-05-05 18:05:49 +0200

    adding missions controller

---
+++ WarsawCityGameServer/WarsawCityGamesServer.Models/Missions/SetCurrentMissionDto.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WarsawCityGamesServer.Models.Missions
+{
+    public class SetCurrentMissionDto
+    {
+        [Required]
+        public string UserName { get; set; }
+
+        [Required]
+        public string MissionName { get; set; }
+    }
+}
---
+++ WarsawCityGameServer/WarsawCityGamesServer.Services/Controllers/MissionController.cs
@@ -0,0 +1,52 @@
+﻿using System.Linq;
+using System.Threading.Tasks;
+using System.Web.Http;
+using AutoMapper;
+using Microsoft.AspNet.Identity;
+using WarsawCityGamesServer.Common.Enums;
+using WarsawCityGamesServer.Entities.Context;
+using WarsawCityGamesServer.Entities.Entities;
+using WarsawCityGamesServer.Models.Missions;
+
+namespace WarsawCityGamesServer.Services.Controllers
+{
+    [Authorize]
+    [RoutePrefix("Mission")]
+    public class MissionController : ApiController
+    {
+        private readonly CityGamesContext context;
+        private readonly IMapper mapper;
+
+        public MissionController(CityGamesContext context, IMapper mapper)
+        {
+            this.context = context;
+            this.mapper = mapper;
+        }
+
+        [Route("SetCurrentMission"), HttpPost]
+        public IHttpActionResult SetCurrentMission(SetCurrentMissionDto mission)
+        {
+            if (mission == null || !ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            var currentMission = context.Missions.FirstOrDefault(x => x.Name == mission.MissionName);
+            if(currentMission == null)
+            {
+                return BadRequest();
+            }
+
+            var player = context.Players.FirstOrDefault(x => x.Name == mission.UserName);
+            if (player == null)
+            {
+                return BadRequest();
+            }
+
+            player.CurrentMission = currentMission;
+            context.SaveChanges();
+            return Ok();
+        }
+ 
+    }
+}
