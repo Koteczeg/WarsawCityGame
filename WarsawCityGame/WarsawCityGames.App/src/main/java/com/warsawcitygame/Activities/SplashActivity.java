d247f0111656d0ca449434b65474afd64adb2b3f
Tygrysku <duszakp@student.mini.pw.edu.pl>
2016-05-30 00:50:56 +0200

    adjusting splash screen timeout

--- WarsawCityGame/WarsawCityGames.App/src/main/java/com/warsawcitygame/Activities/SplashActivity.java
+++ WarsawCityGame/WarsawCityGames.App/src/main/java/com/warsawcitygame/Activities/SplashActivity.java
@@ -47,7 +47,7 @@ public class SplashActivity extends Activity
     private void setPostDelayLogoAnimation(Animation fadeOut)
     {
         splashElementsLayout.startAnimation(fadeOut);
-        int SPLASH_TIME_OUT = 5000;
+        int SPLASH_TIME_OUT = 2000;
         new Handler().postDelayed(new Runnable()
         {
 
