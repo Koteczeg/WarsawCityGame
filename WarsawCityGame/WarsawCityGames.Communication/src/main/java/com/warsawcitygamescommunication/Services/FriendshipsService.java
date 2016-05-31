8ee8fbf4a3f6013327a12ccc51026e0b157c491e
Tygrysku <duszakp@student.mini.pw.edu.pl>
2016-06-01 01:49:02 +0200

    client side add/remove friendship services

--- WarsawCityGame/WarsawCityGames.Communication/src/main/java/com/warsawcitygamescommunication/Services/FriendshipsService.java
+++ WarsawCityGame/WarsawCityGames.Communication/src/main/java/com/warsawcitygamescommunication/Services/FriendshipsService.java
@@ -1,5 +1,6 @@
 package com.warsawcitygamescommunication.Services;
 
+import com.squareup.okhttp.Response;
 import com.warsawcitygames.models.friends_models.FriendModel;
 
 import java.util.List;
@@ -14,4 +15,8 @@ public interface FriendshipsService
     Call<List<FriendModel>> GetFriends();
     @POST("Friendships/FindFriend")
     Call<FriendModel> FindFriend(@Query("username") String username);
+    @POST("Friendships/AssignFriend")
+    Call<Response> AssignFriend(@Query("playerId") int playerId);
+    @POST("Friendships/RemoveFriend")
+    Call<Response> RemoveFriend(@Query("playerId") int playerId);
 }
