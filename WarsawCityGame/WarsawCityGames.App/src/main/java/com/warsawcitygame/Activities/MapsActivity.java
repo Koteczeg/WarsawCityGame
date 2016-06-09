aa78b33f82e822c16e3b89a947250975e6e63c55
Tygrysku <duszakp@student.mini.pw.edu.pl>
2016-06-10 01:35:52 +0200

    maps injection

--- WarsawCityGame/WarsawCityGames.App/src/main/java/com/warsawcitygame/Activities/MapsActivity.java
+++ WarsawCityGame/WarsawCityGames.App/src/main/java/com/warsawcitygame/Activities/MapsActivity.java
@@ -1,5 +1,6 @@
 package com.warsawcitygame.Activities;
 
+import android.content.SharedPreferences;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.os.Handler;
@@ -15,6 +16,7 @@ import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 import com.google.android.gms.maps.model.PolylineOptions;
 import com.warsawcitygame.R;
+import com.warsawcitygame.Utils.MyApplication;
 
 import org.json.JSONArray;
 import org.json.JSONException;
@@ -30,15 +32,20 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 
+import javax.inject.Inject;
+
 public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
 {
     private GoogleMap mMap;
+    @Inject
+    SharedPreferences preferences;
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_maps);
+        ((MyApplication) getApplication()).getServicesComponent().inject(this);
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
