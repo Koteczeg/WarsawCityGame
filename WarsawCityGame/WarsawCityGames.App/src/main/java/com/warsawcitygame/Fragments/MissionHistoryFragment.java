591a28cd3a65ae7a38b07c0f830e17d87f6139e6
Mateusz Bąkała <bakala12@o2.pl>
2016-06-09 20:13:11 +0200

    History

--- WarsawCityGame/WarsawCityGames.App/src/main/java/com/warsawcitygame/Fragments/MissionHistoryFragment.java
+++ WarsawCityGame/WarsawCityGames.App/src/main/java/com/warsawcitygame/Fragments/MissionHistoryFragment.java
@@ -1,29 +1,37 @@
 package com.warsawcitygame.Fragments;
 
+import android.app.Dialog;
 import android.content.Context;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.squareup.okhttp.ResponseBody;
+import com.warsawcitygame.Adapters.MissionHistoryAdapter;
 import com.warsawcitygame.R;
+import com.warsawcitygame.Utils.CustomCallback;
+import com.warsawcitygame.Utils.DialogUtils;
 import com.warsawcitygame.Utils.MyApplication;
+import com.warsawcitygames.models.MissionHistoryModel;
 import com.warsawcitygamescommunication.Services.MissionHistoryService;
+
+import java.util.LinkedList;
+import java.util.List;
 
 import javax.inject.Inject;
 
-/**
- * A simple {@link Fragment} subclass.
- * Activities that contain this fragment must implement the
- * {@link MissionHistoryFragment.OnFragmentInteractionListener} interface
- * to handle interaction events.
- * Use the {@link MissionHistoryFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
+import retrofit.Call;
+import retrofit.Response;
+import retrofit.Retrofit;
+
 public class MissionHistoryFragment extends android.app.Fragment{
-    private OnFragmentInteractionListener mListener;
+
+    private MissionHistoryAdapter adapter;
+    private RecyclerView listView;
 
     public MissionHistoryFragment() {
         // Required empty public constructor
@@ -42,38 +50,44 @@ public class MissionHistoryFragment extends android.app.Fragment{
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_mission_history2, container, false);
+        View view = inflater.inflate(R.layout.fragment_mission_history2, container, false);
+        listView = (RecyclerView)view.findViewById(R.id.rv);
+        getData();
+        return view;
     }
 
-    @Override
-    public void onAttach(Context context) {
-        super.onAttach(context);
-        if (context instanceof OnFragmentInteractionListener) {
-            mListener = (OnFragmentInteractionListener) context;
-        } else {
-            throw new RuntimeException(context.toString()
-                    + " must implement OnFragmentInteractionListener");
-        }
-    }
+    protected void getData() {
+        Call<List<MissionHistoryModel>> call = service.getHistory();
+        final Dialog dialog = DialogUtils.RaiseDialogLoading(getActivity());
+        call.enqueue(new CustomCallback<List<MissionHistoryModel>>(getActivity()) {
+            @Override
+            public void onSuccess(List<MissionHistoryModel> model) {
+                dialog.dismiss();
+            }
 
-    @Override
-    public void onDetach() {
-        super.onDetach();
-        mListener = null;
-    }
+            @Override
+            public void onFailure(Throwable t) {
+                super.onFailure(t);
+                dialog.dismiss();
+            }
 
-    /**
-     * This interface must be implemented by activities that contain this
-     * fragment to allow an interaction in this fragment to be communicated
-     * to the activity and potentially other fragments contained in that
-     * activity.
-     * <p/>
-     * See the Android Training lesson <a href=
-     * "http://developer.android.com/training/basics/fragments/communicating.html"
-     * >Communicating with Other Fragments</a> for more information.
-     */
-    public interface OnFragmentInteractionListener {
-        // TODO: Update argument type and name
-        void onFragmentInteraction(Uri uri);
+            @Override
+            public void onResponse(Response<List<MissionHistoryModel>> response, Retrofit retrofit) {
+                List<MissionHistoryModel> list = new LinkedList<>();
+                if(response!=null){
+                    int c = response.code();
+                    dialog.dismiss();
+                    if(response.isSuccess()){
+                        if(response.body() != null){
+                            for(MissionHistoryModel m: response.body()){
+                                list.add(m);
+                            }
+                        }
+                    }
+                }
+                adapter = new MissionHistoryAdapter(list);
+                listView.setAdapter(adapter);
+            }
+        });
     }
 }
