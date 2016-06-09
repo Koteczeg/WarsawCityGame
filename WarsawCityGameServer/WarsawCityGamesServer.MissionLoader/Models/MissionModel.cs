d83476f0ef972e42fef5f82318d42050fb01b3d1
Tygrysku <duszakp@student.mini.pw.edu.pl>
2016-06-09 21:09:53 +0200

    restoring working fradles

---
+++ WarsawCityGameServer/WarsawCityGamesServer.MissionLoader/Models/MissionModel.cs
@@ -0,0 +1,13 @@
+﻿namespace WarsawCityGamesServer.MissionLoader.Models
+{
+    public class MissionModel
+    {
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string Image { get; set; }
+        public int MinLevel { get; set; }
+        public int ExpReward { get; set; }
+        public int Place { get; set; }
+
+    }
+}
